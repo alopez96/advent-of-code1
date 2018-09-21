09dfd0be70a627b9869f9e9d6fbf6a2911892eb6
Arturo <alopez96@ucsc.edu>
2018-09-21 10:53:05 -0700

    solution to number 1

--- script.js
+++ script.js
@@ -3,3 +3,38 @@
 // ) Santa goes down 1 floor
 
 //question 2 - When does santa first enter the basement
+
+
+const fs = require('fs');
+//solution to 1
+function finalFloor() {
+  console.time('execution time');
+  fs.readFile('./santa.txt', (err, data) => {
+  if(err) {
+    console.log('error', err);
+  }
+  //get all strings
+  const strings = data.toString();
+  // console.log('santa.txt: ', strings);
+
+  //break up the strings
+  const stringsArray = strings.split('');
+
+  console.log('total steps', stringsArray.length);
+  floor = 0;
+  for(i = 0; i < stringsArray.length; i++){
+    if(stringsArray[i] === '('){
+      floor = floor + 1;
+    }
+    else if(stringsArray[i] === ')'){
+      floor = floor - 1;
+    }
+  }
+  console.log('Santa ends up on floor:', floor);
+
+})
+
+console.timeEnd('execution time');
+}
+
+finalFloor();
